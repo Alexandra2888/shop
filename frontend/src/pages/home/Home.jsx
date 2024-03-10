9d69f74c4e09a4a4b65bf888d8658d524f3fe29e
AlexandraM <76844097+Alexandra2888@users.noreply.github.com>
2024-03-10 13:47:15 +0200

    Implement search feature.

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -11,7 +11,8 @@ import { useSearchParams } from "react-router-dom";
 const Home = () => {
   let [searchParams] = useSearchParams();
   const page = searchParams.get("page") || 1;
-  const params = { page };
+  const keyword = searchParams.get("keyword") || "";
+  const params = { page, keyword };
 
   const { data, isLoading, error, isError } = useGetProductsQuery(params);
 
@@ -21,6 +22,9 @@ const Home = () => {
     }
   }, [isError]);
 
+  const columnSize = keyword ? 4 : 3;
+
+
   if (isLoading) return <Loader />;
 
   return (
@@ -29,14 +33,16 @@ const Home = () => {
       <div className="row">
         <div className="col-6 col-md-12">
           <h1 id="products_heading" className="text-secondary">
-            Latest Products
+          {keyword
+              ? `${data?.products?.length} Products found with keyword: ${keyword}`
+              : "Latest Products"}
           </h1>
 
           <section id="products" className="mt-5">
             <div className="row">
               {data?.products?.map((product) => (
                 // eslint-disable-next-line react/jsx-key
-                <ProductItem product={product} />
+                <ProductItem product={product} columnSize={columnSize} />
               ))}
             </div>
           </section>
